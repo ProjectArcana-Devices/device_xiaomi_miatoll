21ec96d0f23ae7f37aba4885f44d91451c1465df
HaSSaN <hasun.mohl@gmail.com>
2022-03-17 02:12:41 +0000

    Arcana-ify

--- aosp_miatoll.mk
+++ aosp_miatoll.mk
@@ -7,18 +7,17 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
+# Inherit some common ArcaneOS stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
-# Quick Tap
-TARGET_SUPPORTS_QUICK_TAP := true
+# Prebuilt apps
+$(call inherit-product-if-exists, vendor/prebuilt-apps/config.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := aosp_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
@@ -26,8 +25,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
+# Inherit some props from Arcana
+ARCANA_OFFICIAL := true
+ARCANA_DEVICE := miatoll
+ARCANA_MAINTAINER := HaSSaN
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_BUILD_GRAPHENEOS_CAMERA := true
+PREBUILT_LAWNCHAIR := true
+COPY_APN_SYSTEM := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
